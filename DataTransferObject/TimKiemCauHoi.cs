b7bd6a655c215065ba6bb405e73ea25c9babfae6
turo151001 <111428841+turo1501@users.noreply.github.com>
2023-08-30 20:23:38 +0700

    Add files via upload

---
+++ DataTransferObject/TimKiemCauHoi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObject
+{
+    public class TimKiemCauHoi
+    {
+        public string KeyCauHoi { get; set; }
+        public string KeyDapAn { get; set; }
+        public string DapAn { get; set; }
+        public int MaChuong { get; set; }
+
+        public TimKiemCauHoi(string keyCauHoi, string keyDapAn, string dapAn, int chuong)
+        {
+            KeyCauHoi = keyCauHoi;
+            KeyDapAn = keyDapAn;
+            DapAn = dapAn;
+            MaChuong = chuong;
+        }
+
+        public TimKiemCauHoi() { }
+    }
+}
---
+++ DataTransferObject/TimKiemSinhVien.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataTransferObject
+{
+    public class TimKiemSinhVien
+    {
+        public string MSSV { get; set; }
+        public string HoDem { get; set; }
+        public string Ten { get; set; }
+        public string MaLop { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public byte GioiTinh { get; set; }
+        public string DiaChi { get; set; }
+
+        public TimKiemSinhVien(string mSSV, string hoDem, string ten, string maLop, DateTime ngaySinh, byte gioiTinh, string diaChi)
+        {
+            MSSV = mSSV;
+            HoDem = hoDem;
+            Ten = ten;
+            MaLop = maLop;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            DiaChi = diaChi;
+        }
+
+        public TimKiemSinhVien() { }
+    }
+}
